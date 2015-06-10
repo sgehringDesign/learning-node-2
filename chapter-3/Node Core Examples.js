be5ce7c05caf0def94ced3c609685cd7d7ebed9e
Stephen Gehring <sgehringdesign@gmail.com>
2015-06-09 23:57:08 -0700

    adding first chapter 3 example

--- chapter-3/Node Core Examples.js
+++ chapter-3/Node Core Examples.js
@@ -2,3 +2,15 @@
 CHAPTER 3
 NODE CORE
 */
+
+/*
+    testing differences between node global and clinetside global 
+*/
+
+var test = "this really isn't global, as we know global";
+
+/* 
+    console.log(global); 
+*/
+    
+    
